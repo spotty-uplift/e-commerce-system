f399db2ae37b603d19918a224db1b5a4a6b9b354
Maria Santos <153066368+spotty-uplift@users.noreply.github.com>
2023-12-07 13:00:07 +0700

    Create Checkout.cpp

---
+++ Checkout.cpp
@@ -0,0 +1,9 @@
+// Checkout.cpp
+#include "Checkout.h"
+#include <iostream>
+
+void Checkout::processCheckout(const ShoppingCart& cart) {
+    // Implement checkout logic here
+    // Validate data, update the database, etc.
+    std::cout << "Checkout processed successfully!\n";
+}
